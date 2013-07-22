c61d065d0d96eaf76f925d5f1c6b13de63b9b3f6
Philip Forget <philipforget@gmail.com>
2013-07-21 20:47:07 -0400

    basic framed messaging

--- coffee.arduino/coffee.ino
+++ coffee.arduino/coffee.ino
@@ -1,45 +1,62 @@
-#include <LM75A.h>
-#include <SoftwareSerial.h>
-#include <Wire.h>
-
-// Max serial speed between arduino and ipod touch
-# define SERIAL_SPEED 19200
-# define BCAST_INTERVAL 500
-
-LM75A lm(79);
-SoftwareSerial server_serial(12, 13);
-long b_previous_m = 0;
+char cmd0, cmd1, cmd2, cmd3, cmd4, cmd5, cmd6, cmd7;
+int past_available = 0;
 
 void setup(){
-  Wire.begin();
-
-  Serial.begin(SERIAL_SPEED);
+  Serial.begin(57600);
   Serial.println("Starting up");
 }
 
 
-float get_temp(){
-  return lm.get_temp();
+void drain_serial(){
+    while(Serial.available())
+      if(Serial.read() == '#')
+        return;
 }
 
+void process_message(){
+  // Process a fixed-frame message
+  cmd0 = Serial.read();
+  if(cmd0 != '%') {
+    Serial.println("Message frame did not start with %");
+    // Drain any remaining message parts
+    return drain_serial();
+  }
 
-void broadcast_temperature(){
-  unsigned long current_m = millis();
+  cmd1 = Serial.read();
+  cmd2 = Serial.read();
+  cmd3 = Serial.read();
+  cmd4 = Serial.read();
+  cmd5 = Serial.read();
+  cmd6 = Serial.read();
+  cmd7 = Serial.read();
 
-  if( current_m - b_previous_m > BCAST_INTERVAL){
-    b_previous_m = current_m;
+  if(cmd7 != '#'){
+    Serial.println("Invalid frame ending");
+    return drain_serial();
+  }
 
-    Serial.println(get_temp());
+  Serial.println("Received Frame");
+
+  switch(cmd1){
+    // Set
+    case 'S':
+      switch(cmd2){
+
+      }
+    // Get
+    case 'G':
+      switch(cmd2){
+        case 'T':
+          return get_temperature();
+      }
+    default:
+      Serial.println("No match for message");
   }
 }
 
 
 void loop(){
-  broadcast_temperature();
-
-  // Check for incoming commands from the server
-  if(server_serial.available()){
-    Serial.print("Got a message from server");
-    Serial.write(server_serial.read());
+  if(Serial.available() >= 8){
+    process_message();
   }
 }
