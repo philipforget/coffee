fba4f03887f47acac1a3f3eec27a3bcc6b72f41f
Philip Forget <philipforget@gmail.com>
2013-08-18 19:45:02 -0400

    arduino changes

--- serial_trials.arduino/serial_trials.ino
+++ serial_trials.arduino/serial_trials.ino
@@ -1,32 +1,13 @@
 #define print Serial.print
 #define println Serial.println
 
-int intValue = 258;
-// 258 in hex is 0x102
-// 258 in bin is 100000010
-
 void setup(){
   Serial.begin(9600);
   println("Starting up");
 }
 
 void loop(){
-  int loWord, hiWord;
-  byte loByte, hiByte;
+  println("Hello there python, this is arduino");
 
-  hiByte = highByte(intValue);
-  loByte = lowByte(intValue);
-
-  print("decimal: ");
-  println(intValue, DEC);
-  print("hex    : ");
-  println(intValue, HEX);
-  println();
-
-  print("low    : ");
-  println(loByte, DEC);
-  print("high   : ");
-  println(hiByte, DEC);
-
-  delay(10000);
+  delay(1000);
 }
