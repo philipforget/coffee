e4b7d5dde9689f655750cb2ae420daf883fa310c
Philip Forget <philipforget@gmail.com>
2013-08-26 19:27:48 -0400

    agnostic makefile

--- serial_trials.arduino/serial_trials.ino
+++ serial_trials.arduino/serial_trials.ino
@@ -1,13 +1,18 @@
 #define print Serial.print
 #define println Serial.println
 
+#include <SoftwareSerial.h>
+
+SoftwareSerial ss(12,13);
+
 void setup(){
   Serial.begin(9600);
   println("Starting up");
 }
 
 void loop(){
-  println("Hello there python, this is arduino");
-
-  delay(1000);
+  println("High");
+  delay(5000);
+  println("Low");
+  delay(5000);
 }
